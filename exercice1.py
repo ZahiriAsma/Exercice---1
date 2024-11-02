a96591d41a0a071c08fb2bfb8f6c73ec81760e20
ZahiriAsma <assmaezahiri697@gmail.com>
2024-11-02 18:13:48 +0100

    Add files via upload

---
+++ exercice1.py
@@ -0,0 +1,14 @@
+tab1=[]
+tab2=[]
+tab3=[]
+s=0
+for i in range(9):
+    n=int (input("donne un nombre n"))
+    tab1.append(n)
+    x=int (input("donne un nombre x"))
+    tab2.append(x)
+    s=n+x
+    tab3.append(s)
+print(tab1)
+print(tab2)
+print(tab3)
